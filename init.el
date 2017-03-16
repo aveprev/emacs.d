c2f32ccaf0851e4b91e7ae0fe151d2a9b74444ac
Andrei Veprev <andrei.veprev@gmail.com>
2017-03-17 01:28:07 +0300

    Fresh basics with powerline and moe theme enabled

---
+++ init.el
@@ -0,0 +1,45 @@
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+(require 'package)
+(add-to-list
+ 'package-archives
+ '("melpa" . "http://melpa.org/packages/")
+ t)
+(package-initialize)
+
+(desktop-save-mode t)
+(setq ns-pop-up-frames nil)
+(setq echo-keystrokes 0.1)
+(setq make-backup-files nil)
+(setq-default indent-tabs-mode nil)
+(show-paren-mode t)
+
+(setq linum-format "%4d ")
+(add-hook 'find-file-hook 'linum-mode)
+
+(require 'tramp)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+(require 'saveplace)
+(setq-default save-place t)
+
+(require 'powerline)
+(require 'moe-theme)
+
+(setq moe-theme-highlight-buffer-id t)
+
+(powerline-moe-theme)
+(moe-theme-set-color 'green)
+(moe-dark)
+
+(ido-mode 1)
+(ido-everywhere 1)
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode 1)
