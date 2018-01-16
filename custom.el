cb8f0ba41cf6a4294360cb49a3adff41bb6802dd
Andrei Veprev <andrei.veprev@gmail.com>
2018-01-16 14:31:24 +0300

    Markdow mode package

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (nimbus-theme apache-mode toml-mode nginx-mode yaml-mode dockerfile-mode web-mode use-package ensime magit diff-hl ido-ubiquitous moe-theme powerline))))
+    (markdown-mode nimbus-theme apache-mode toml-mode nginx-mode yaml-mode dockerfile-mode web-mode use-package ensime magit diff-hl ido-ubiquitous moe-theme powerline))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
